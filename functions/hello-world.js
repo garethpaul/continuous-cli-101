5e06f5287b93166db82134efcdacdaeb35870c61
Gareth Paul Jones <gareth@garethpaul.com>
2021-02-02 18:34:57 -0800

    adding update from 101 training

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
   const result = {
-    message: "Hello CLI 101 Training"
+    message: "Hello CLI 101 Training from Wednesday"
   }
 
   callback(null, result);
